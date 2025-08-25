af134867e2c5f61d7fea60b2753fed17636d0251
MPC88 <myemail@gmail.com>
2025-08-25 16:14:49 +0100

    Project settings corrections.

--- MC_SVTraderRework/Properties/AssemblyInfo.cs
+++ MC_SVTraderRework/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MC_SVTraderItemReserve")]
+[assembly: AssemblyTitle("MC_SVTraderRework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MC_SVTraderItemReserve")]
+[assembly: AssemblyProduct("MC_SVTraderRework")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
